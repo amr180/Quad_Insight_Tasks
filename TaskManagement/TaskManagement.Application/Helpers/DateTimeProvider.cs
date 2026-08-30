81a16618d4e5cd2ef7ea5222282a5f84bbdec11a
amr180 <amrr00360@gmail.com>
2026-08-30 11:08:38 +0300

    fix DatetimeProvider

--- TaskManagement/TaskManagement.Application/Helpers/DateTimeProvider.cs
+++ TaskManagement/TaskManagement.Application/Helpers/DateTimeProvider.cs
@@ -5,4 +5,10 @@ namespace TaskManagement.Application.Helpers;
 public class DateTimeProvider : IDateTimeProvider
 {
     public DateTime UtcNow => DateTime.UtcNow;
+
+    public DateTime ConvertToUserLocalTime(DateTime utcDateTime, string timeZoneId)
+    {
+        var userTimeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+        return TimeZoneInfo.ConvertTimeFromUtc(utcDateTime, userTimeZone);
+    }
 }
