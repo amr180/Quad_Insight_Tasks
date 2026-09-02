cc5a3be4e3957043a4a8b165dc947399a39b57ab
amr180 <amrr00360@gmail.com>
2026-09-02 16:39:29 +0300

    generate Components and pages

---
+++ Task_Management_System/src/app/pages/completed-tasks/completed-tasks.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-completed-tasks',
+  styleUrl: './completed-tasks.css',
+  templateUrl: './completed-tasks.html',
+})
+export class CompletedTasks {}
---
+++ Task_Management_System/src/app/pages/view-users/view-users.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-view-users',
+  styleUrl: './view-users.css',
+  templateUrl: './view-users.html',
+})
+export class ViewUsers {}
